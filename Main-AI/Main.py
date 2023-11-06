d45245429df5ecf290a51d900b3377f7d8776f6c
Hamish Leahy <98940688+Hamish-Leahy@users.noreply.github.com>
2023-11-06 13:37:45 +1100

    Added explainers to the code

--- Main-AI/Main.py
+++ Main-AI/Main.py
@@ -1,19 +1,32 @@
-# this is a learner version of the main.py file feel free to change it or even completely re-do it, enjoy
+# This is a simplified version of a chatbot implemented in Python.
+# The chatbot uses a basic response mechanism and can be terminated by typing 'bye'.
 
-# add the training set in here for all of the files sytem codes and more 
-
+# Import the 'responses' module, which contains a set of predefined responses.
 import responses
 
+# Define the chatbot function, which manages the conversation.
 def chatbot():
+    # Print a welcome message to the user.
     print("Simple Chatbot - Type 'bye' to exit")
-    
+
+    # Start an infinite loop for continuous interaction with the user.
     while True:
+        # Get user input and store it in the 'user_input' variable.
         user_input = input("You: ")
+        
+        # Check if the user wants to exit the chat by typing 'bye'.
         if user_input.lower() == "bye":
+            # If the user says 'bye', print a farewell message and exit the loop.
             print("Chatbot: Goodbye!")
             break
+
+        # Use the 'responses' module to get a response based on the user's input.
         response = responses.get_response(user_input)
+        
+        # Display the chatbot's response to the user.
         print(f"Chatbot: {response}")
 
+# Check if this script is being run directly (not imported as a module).
 if __name__ == "__main__":
+    # Call the 'chatbot' function to start the chatbot interaction.
     chatbot()
