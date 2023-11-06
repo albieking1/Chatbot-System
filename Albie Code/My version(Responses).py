22b9477951f8c5ebea7480325fb49679bf636c37
Ambush <129244078+albieking1@users.noreply.github.com>
2023-11-06 18:13:28 +1100

    Update My version(Responses).py
    Getting somewhere

--- Albie Code/My version(Responses).py
+++ Albie Code/My version(Responses).py
@@ -6,3 +6,5 @@ def get_response(user_input):
 greetings = ["hello", "hi", "hey"]
 farewells = ["bye", "goodbye", "see you later"]
 
+if any(word in user_input for word in greetings):
+    return "Hello, I am a chatbot. How may I assist you?"
