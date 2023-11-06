75dd5b698c170a20824682c90e33c9822afd543e
Ambush <129244078+albieking1@users.noreply.github.com>
2023-11-06 18:14:37 +1100

    Update My version(Responses).py
    Almost

--- Albie Code/My version(Responses).py
+++ Albie Code/My version(Responses).py
@@ -8,3 +8,5 @@ farewells = ["bye", "goodbye", "see you later"]
 
 if any(word in user_input for word in greetings):
     return "Hello, I am a chatbot. How may I assist you?"
+elif any(word in user_input for word in farewells):
+    return "Goodbye!"
